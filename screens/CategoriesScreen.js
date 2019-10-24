08ebbf7e84a1f3e05da17aa8c3547015afff5308
Patrick McMahon <pmcmahon14@gmail.com>
2019-10-23 18:04:26 -0700

    grid layout set up

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
+
+import {CATEGORIES} from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+    return (
+        <View style={styles.gridItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    );
+};
 
 const CategoriesScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>Categories</Text>
-            <Button title='Go to Meals' onPress={() => {
-            props.navigation.navigate({
-                routeName: 'CategoryMeals'
-            });
-            }} />
-
-        </View>
+        <FlatList
+            keyExtractor={(item, index) => item.id}
+            data={CATEGORIES}
+            renderItem={renderGridItem}
+            numColumns={2}/>
     );
 };
 
@@ -20,7 +26,13 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 100
     }
+
 });
 
 export default CategoriesScreen;
