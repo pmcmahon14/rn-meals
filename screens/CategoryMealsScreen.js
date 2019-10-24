5fd34bb43f945f78147aeea2c44f2173f20a0401
Patrick McMahon <pmcmahon14@gmail.com>
2019-10-24 14:10:57 -0700

    set dynamic navigation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
 
-import {CATEGORIES} from "../data/dummy-data";
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
-const CategoryMealsScreen = props => {
+const CategoryMealScreen = props => {
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id ===catId);
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
     return (
         <View style={styles.screen}>
-            <Text>CategoryMealScreen</Text>
+            <Text>The Category Meal Screen!</Text>
             <Text>{selectedCategory.title}</Text>
-            <Button title='Go to Details' onPress={() => {
-                props.navigation.navigate({
-                    routeName: 'MealDetail'
-                });
-            }} />
-            <Button title='Go Back' onPress={() => {
-            props.navigation.goBack();
-            }} />
+            <Button
+                title="Go to Details"
+                onPress={() => {
+                    props.navigation.navigate({
+                        routeName: 'MealDetail'
+                    });
+                }}
+            />
+            <Button
+                title="Go Back"
+                onPress={() => {
+                    props.navigation.pop();
+                }}
+            />
         </View>
     );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
@@ -32,4 +53,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
