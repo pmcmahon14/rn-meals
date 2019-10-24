7edaf570451e80290bfbe578bbe34265d9969ea1
Patrick McMahon <pmcmahon14@gmail.com>
2019-10-23 18:58:42 -0700

    added navigation option to header

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {
+    View,
+    Text,
+    FlatList,
+    StyleSheet,
+    TouchableOpacity,
+    Platform
+} from 'react-native';
 
-import {CATEGORIES} from "../data/dummy-data";
-
-const renderGridItem = (itemData) => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>
-    );
-};
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
+    const renderGridItem = itemData => {
+        return (
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                    props.navigation.navigate({ routeName: 'CategoryMeals' });
+                }}
+            >
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
         <FlatList
             keyExtractor={(item, index) => item.id}
             data={CATEGORIES}
             renderItem={renderGridItem}
-            numColumns={2}/>
+            numColumns={2}
+        />
     );
+};
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -32,7 +56,6 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 100
     }
-
 });
 
 export default CategoriesScreen;
